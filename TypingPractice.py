d049fbbf757d52b1ef9c5417adc9b3fdfb232111
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2023-08-21 17:24:30 +0330

    Enhancement: Improve clarity by renaming 'char_list' to 'practice_set

--- TypingPractice.py
+++ TypingPractice.py
@@ -10,37 +10,37 @@ SIGNS = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+','`
 
 
 def menu_start():
-    char_list = []
+    practice_set = []
     print(colorama.Fore.MAGENTA + "welcome\nchoose one of the options below:") ## print welcome message
     print(colorama.Fore.CYAN + "\t0.numbers\n\t1.signs\n\t2.both of them\n\t3.custom list\n\t4.exit") ## print menu options
     menu_selection = int(input())
 
     if menu_selection == 0:
-        char_list = NUMBERS
+        practice_set = NUMBERS
     elif menu_selection == 1:
-        char_list = SIGNS
+        practice_set = SIGNS
     elif menu_selection == 2: 
-        char_list = NUMBERS + SIGNS
+        practice_set = NUMBERS + SIGNS
     elif menu_selection == 3:
         print(colorama.Fore.BLUE + "type finish, if you don't want add new char")
-        char_list = []
+        practice_set = []
         while(True):
             char_input = input()
             if char_input == "finish":
                 break
-            char_list.append(char_input)
-            print(colorama.Fore.BLUE + str(char_list))
-        print(colorama.Fore.YELLOW + '\nyou chose ' + str(char_list))
+            practice_set.append(char_input)
+            print(colorama.Fore.BLUE + str(practice_set))
+        print(colorama.Fore.YELLOW + '\nyou chose ' + str(practice_set))
     elif menu_selection == 4:
         exit()
 
     print(f"for back to menu, type {colorama.Fore.RED}finish\n")
-    return char_list
+    return practice_set
 
 def main():
     while True:
-        char_list = menu_start()
-        rand_char = random.choice(char_list)
+        practice_set = menu_start()
+        rand_char = random.choice(practice_set)
         while True:
             print(rand_char)
             data = input()
@@ -48,7 +48,7 @@ def main():
                 break
             elif data == rand_char:
                 print('\033[5;32m\u2713 \033[0;0m')
-                rand_char = random.choice(char_list)
+                rand_char = random.choice(practice_set)
             else:
                 print('\033[5;91mX \033[0;0m')
 
