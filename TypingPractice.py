521345e168444775bd0ce6c61faf249846d0f965
Mostafa Ebrahimi <mostafa.ebrahimi2002@gmail.com>
2023-08-28 12:39:53 +0330

    Feature: Enable specifying string length for random output

--- TypingPractice.py
+++ TypingPractice.py
@@ -14,29 +14,29 @@ SIGNS = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '
 def get_custom_char_set():
     print(colorama.Fore.BLUE + "Type 'finish' if you don't want to add new characters")
     custom_set = []
-    
+
     while True:
         char_input = input()
-        
+
         if char_input == "finish":
             if not custom_set:
                 print(f"{colorama.Fore.RED}You must enter at least one character before finishing.")
                 continue
             break
-        elif char_input == "":
+        elif not char_input:
             print(f"{colorama.Fore.RED}Oops! It looks like you didn't enter any characters. Please provide at least one character.")
             continue
-        
+
         custom_set.append(char_input)
         print(colorama.Fore.BLUE + str(custom_set))
-    
+
     print(colorama.Fore.YELLOW + '\nYou chose ' + str(custom_set))
     return custom_set
 
 def menu_start():
     print(colorama.Fore.MAGENTA + "Welcome!\nChoose one of the options below:")
     print(colorama.Fore.CYAN + "\t0. Numbers\n\t1. Signs\n\t2. Both of them\n\t3. Custom list\n\t4. Exit")
-    
+
     menu_selection = int(input())
     practice_set = []
 
@@ -51,23 +51,33 @@ def menu_start():
     elif menu_selection == 4:
         exit()
 
+    print(colorama.Fore.CYAN + "Enter the size of random word:")
+    size = int(input())
+    while size <= 0:
+        print(colorama.Fore.RED + "Size must be a positive integer.")
+        size = int(input())
+
     print(f"For back to the menu, type {colorama.Fore.RED}'finish'\n")
-    return practice_set
+    return practice_set, size
+
+def generate_rand_string(input_set, size):
+    chosen_element = random.choices(input_set, k=size)
+    return ''.join(chosen_element)
 
 def main():
     while True:
-        practice_set = menu_start()
-        rand_char = random.choice(practice_set)
-        
+        practice_set, size = menu_start()
+        rand_string = generate_rand_string(practice_set, size)
+
         while True:
-            print(rand_char)
+            print(rand_string)
             practice_input = input()
-            
+
             if practice_input == 'finish':
                 break
-            elif practice_input == rand_char:
+            elif practice_input == rand_string:
                 print(f"{colorama.Fore.GREEN}{CHECK_MARK}")
-                rand_char = random.choice(practice_set)
+                rand_string = generate_rand_string(practice_set, size)
             else:
                 print(f"{colorama.Fore.RED}X")
 
